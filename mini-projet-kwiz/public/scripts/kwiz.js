afac4db43ae25fb1b11ce42bf47fca9fac1e89b7
SThor <paulgivel@gmail.com>
2018-01-15 23:38:20 +0100

    covered all required items + scoreboard + two step start

--- mini-projet-kwiz/public/scripts/kwiz.js
+++ mini-projet-kwiz/public/scripts/kwiz.js
@@ -1,16 +1,22 @@
 //communication protocol variables
+const EVENT_QUIZ = "quiz";
 const EVENT_CLIENT_COUNT = "client_count";
 const EVENT_PLAYERS = "players";
 const EVENT_LOGGED_IN = "logged_in";
 const EVENT_ANSWER = "answer";
 const EVENT_UPDATE_ANSWERS = "update_answers";
-const EVENT_DESELECT = "deselect";
+const EVENT_EVERY_PLAYER_ANSWERED = "every_player_answered";
+const EVENT_WAITING_PAGE = "waiting_page";
+const EVENT_TEST_NICKNAME = "test_nickname";
+const EVENT_NICKNAME_TEST_RESPONSE = "nickname_test_response";
 
 var kwiz;
 kwiz = {
     socket: 'undefined',
-    content: 'undefined'
+    content: 'undefined',
+    quiz: 'undefined'
 };
+
 
 //retrieve UI elements and start communication with the server
 kwiz.start = function (socket) {
@@ -19,9 +25,50 @@ kwiz.start = function (socket) {
 
     //init communication
     kwiz.socket = socket;
-    kwiz.socket.on('quiz', kwiz.createQuestionsUiFromData);
+
+    kwiz.socket.on(EVENT_WAITING_PAGE, kwiz.createWaitingPage);
+
+    kwiz.socket.on(EVENT_QUIZ, kwiz.createQuestionsUiFromData);
 
     kwiz.socket.on(EVENT_UPDATE_ANSWERS, kwiz.updateAnswers);
+};
+
+kwiz.createWaitingPage = function () {
+    //remove existing components if any in the content
+    while (kwiz.content.firstChild) {
+        kwiz.content.removeChild(kwiz.content.firstChild);
+    }
+
+    var title, counterP;
+    title = document.createElement("h1");
+    title.innerHTML = "Waiting for all players to login <span id='wait'>.</span>";
+    title.setAttribute("class","text-center");
+
+    var clientsCount = document.getElementById("clientCount").innerHTML.substr(0,1);
+    var playersCount = document.getElementById("playersTable").rows.length-1;
+
+    counterP = document.createElement("p");
+    counterP.innerHTML = "Logged-in players: <span id='wait_counter'>"+playersCount+"/"+clientsCount+"</span>";
+    counterP.setAttribute("class","text-center");
+
+    window.dotsGointUp = true;
+    var dots = window.setInterval( function () {
+       var wait = document.getElementById("wait");
+       if(typeof wait !== 'undefined' && wait!== null){
+           if(window.dotsGointUp){
+               wait.innerHTML += ".";
+           }else{
+               wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
+               if(wait.innerHTML === "")
+                   window.dotsGointUp = true;
+           }
+           if( wait.innerHTML.length > 6 )
+               window.dotsGointUp = false;
+       }
+    }, 100);
+
+    kwiz.content.appendChild(title);
+    kwiz.content.appendChild(counterP);
 };
 
 //use a description object to create the UI
@@ -33,8 +80,8 @@ kwiz.createQuestionsUiFromData = function (data) {
     }
 
     //extract some content from the data
-    var quiz = data.quiz; //quiz is an array of questions
-    var nbQuestions = quiz.length;
+    kwiz.quiz = data.quiz; //quiz is an array of questions
+    var nbQuestions = kwiz.quiz.length;
 
     //variables declaration
 
@@ -44,11 +91,11 @@ kwiz.createQuestionsUiFromData = function (data) {
 
     //variables in the nested loop
     var nbOptions;
-    var option, label, labelID, radio, radioID, br, span, p_answers, p_answersID;
+    var option, formCheckDiv, label, labelID, radio, radioID, br, span, p_answers, p_answersID;
 
     for (let questionIdx = 0; questionIdx < nbQuestions; questionIdx++) {
         //extract content from the question
-        question = quiz[questionIdx];
+        question = kwiz.quiz[questionIdx];
         questionId = question.id;
         title = question.question;
         answer = question.answer;
@@ -67,7 +114,7 @@ kwiz.createQuestionsUiFromData = function (data) {
 
         //create a div that will be a radio button (BootStrap) content
         radioDiv = document.createElement("div");
-        radioDiv.setAttribute("class", "options radio");
+        radioDiv.setAttribute("class", "options radio row");
 
         //iterate over each options to create the radio buttons and labels
         for (let optionID = 0; optionID < nbOptions; optionID++) {
@@ -85,7 +132,7 @@ kwiz.createQuestionsUiFromData = function (data) {
             label.setAttribute('for', radioID);
             label.setAttribute('id', labelID);
             label.setAttribute('name', questionId);
-            label.setAttribute('class', 'option');
+            label.setAttribute('class', 'option form-check-label');
             label.answer = answer;
             label.innerHTML = option;
 
@@ -94,18 +141,26 @@ kwiz.createQuestionsUiFromData = function (data) {
             radio.setAttribute("value", option);
             radio.setAttribute("id", radioID);
             radio.setAttribute("name", questionId); //pour grouper les radio buttons niveau comportement
+            radio.setAttribute("class","form-check-input");
             radio.answer = answer;
             kwiz.createClickListener(radio);
 
             p_answers = document.createElement('p');
             p_answers.setAttribute('id', p_answersID);
+            p_answers.setAttribute('class', 'col-md-5  text-right');
 
             //add a br to change line
             br = document.createElement('br');
 
+            formCheckDiv = document.createElement('div');
+            formCheckDiv.setAttribute("class","form-check col-md-7");
+
+            formCheckDiv.appendChild(radio);
+            formCheckDiv.appendChild(label);
+
+
             //add the elements to the radio div
-            radioDiv.appendChild(radio);
-            radioDiv.appendChild(label);
+            radioDiv.appendChild(formCheckDiv);
             radioDiv.appendChild(p_answers);
             radioDiv.appendChild(br);
         }
@@ -120,18 +175,20 @@ kwiz.createQuestionsUiFromData = function (data) {
 kwiz.createClickListener = function (radio) {
     var questionId = radio.getAttribute('name');
     var option = radio.getAttribute('value');
+    var optionId = radio.getAttribute('id');
 
     radio.onclick = function () {
         var options = document.querySelectorAll("input[type='radio']:checked");
 
         var checkedAnswers = [];
 
-        for(var key in options){
+        for(let key in options){
             if(!options.hasOwnProperty(key)) continue;
             checkedAnswers.push(options[key].attributes.id.value);
         }
 
-        kwiz.socket.emit(EVENT_ANSWER,{uID:kwiz.socket.id, qID:questionId, options:checkedAnswers});
+        kwiz.socket.on(EVENT_EVERY_PLAYER_ANSWERED,kwiz.everyPlayerAnswered);
+        kwiz.socket.emit(EVENT_ANSWER,{uID:kwiz.socket.id, qID:questionId, optionId:optionId, options:checkedAnswers});
     };
 };
 
@@ -140,25 +197,46 @@ kwiz.updateAnswers = function (data) {
     var answers = data['answers'];
 
     //clear answers counters
-    var answersP = document.querySelectorAll(".radio p");
+    var answersP = document.querySelectorAll("#"+data.qID+" .radio p");
     for(var i=0; i<answersP.length; i++){
         var paragraph = answersP[i];
-        paragraph.innerText = '';
+        paragraph.innerHTML = '';
     }
 
 
-    for(var answer in answers){
-        if(!answers.hasOwnProperty(answer)) continue;
+    for(var optionID in answers){
+        if(!answers.hasOwnProperty(optionID) || answers[optionID]=== null) continue;
 
-        var paragraphID = answer.replace("radio","p");
-
+        var paragraphID = 'p_' + data.qID + '_' + optionID;
 
         var text;
-        if(answers[answer]>1){
+        if(answers[optionID]>1){
             text = "joueurs ont choisi cette réponse";
         }else{
             text = "joueur a choisi cette réponse";
         }
-        document.getElementById(paragraphID).innerHTML = '('+answers[answer]+" "+text+')';
+        document.getElementById(paragraphID).innerHTML = '('+answers[optionID]+" "+text+')';
+    }
+};
+
+kwiz.everyPlayerAnswered = function(data){
+    //prevent from changing answer
+    var qID = data.qID;
+
+    var qNumber = data.qID.replace('q','') - 1;
+    question = kwiz.quiz[qNumber];
+
+    for(let optionID = 0; optionID < question.options.length; optionID++){
+        var option = question.options[optionID];
+
+        var radioID = 'radio_' + qID + '_' + optionID;
+        document.getElementById(radioID).disabled = true;
+
+        var labelID = 'label_' + qID + '_' + optionID;
+        if(option === data.answer){
+            document.getElementById(labelID).style.color = "green";
+        }else{
+            document.getElementById(labelID).style.color = "red";
+        }
     }
 };
--- mini-projet-kwiz/server.js
+++ mini-projet-kwiz/server.js
@@ -18,24 +18,28 @@ var io = require('socket.io')(server);
 //data variables
 var players = {};       //logged-in players
 var clientCount = 0;    //all sockets counter
-var answers = {};       //counters for each option of each question
-var questionStatus = {};
+var playersAnswers = {};
 
 for (let questionIdx = 0; questionIdx < (quiz.quiz.length); questionIdx++) {
     //extract content from the question
     question = quiz.quiz[questionIdx];
     questionId = question.id;
 
-    questionStatus[questionId] = {};
+    playersAnswers[questionId] = {};
 }
 
 //communication protocol variables
+const EVENT_QUIZ = "quiz";
 const EVENT_CLIENT_COUNT = "client_count";
 const EVENT_PLAYERS = "players";
 const EVENT_LOGGED_IN = "logged_in";
 const EVENT_ANSWER = "answer";
 const EVENT_UPDATE_ANSWERS = "update_answers";
-const EVENT_DESELECT = "deselect";
+const EVENT_EVERY_PLAYER_ANSWERED = "every_player_answered";
+const EVENT_WAITING_PAGE = "waiting_page";
+const EVENT_TEST_NICKNAME = "test_nickname";
+const EVENT_NICKNAME_TEST_RESPONSE = "nickname_test_response";
+
 
 //server static file in the public directory
 app.use(express.static('public'))
@@ -55,70 +59,126 @@ io.on('connection', function (socket) {
         io.emit(EVENT_CLIENT_COUNT,{'count':clientCount});
         console.log("clients left : "+clientCount);
 
+        //remove player answers from playersAnswers
+        for(let qID=0;qID<quiz.quiz.length;qID++){
+            let question = quiz.quiz[qID];
+            delete playersAnswers[question.id][socket.id];
+            updateAnswersCounters(question.id);
+        }
+
+        //remove player
         delete players[socket.id];
         io.emit(EVENT_PLAYERS,{players:players});
+
+        if(clientCount === Object.keys(players).length){
+            //send the questions to all clients
+            io.emit(EVENT_QUIZ, quiz);
+        }else{
+            socket.emit(EVENT_WAITING_PAGE);
+        }
+    });
+
+    socket.on(EVENT_TEST_NICKNAME, (data) => {
+        testNickname(data,socket);
     });
 
     socket.on(EVENT_LOGGED_IN, (data) => {
-        console.log(data);
         loggedIn(data,socket);
     });
 });
 
+function updateAnswersCounters(qID) {
+    var answersCounters = [];
+    let questionAnswers = playersAnswers[qID];
+    for(let uID in questionAnswers){
+        if(!questionAnswers.hasOwnProperty(uID)) continue;
+
+        let answer = questionAnswers[uID];
+        if(answer in answersCounters){
+            answersCounters[answer]++;
+        }else{
+            answersCounters[answer] = 1;
+        }
+    }
+    io.emit(EVENT_UPDATE_ANSWERS,{qID: qID, answers:answersCounters});
+}
+
 function answered(data, socket) {
-    answers[data.uID]=data.options;
+    let garbage = 'radio_' + data.qID + '_';
+    playersAnswers[data.qID][data.uID] = data.optionId.replace(garbage,'');
 
-    var answersCounters = {};
+    updateAnswersCounters(data.qID);
 
-    for (var key in answers){
-        if(!answers.hasOwnProperty(key)) continue;
-
-        var uAnswers = answers[key];
-
-        for(var i=0; i<uAnswers.length; i++){
-            var radio = uAnswers[i];
-            if(radio in answersCounters){
-                answersCounters[radio]++;
-            }else{
-                answersCounters[radio] = 1;
-            }
+    let nbOfAnswers = 0;
+    for(let i=0; i<Object.keys(players).length; i++){
+        if(typeof (playersAnswers[data.qID][Object.keys(players)[i]]) !== 'undefined'){
+            nbOfAnswers++;
         }
     }
 
-    questionStatus[data.qID][data.uID] = true;
-
-    var everyPlayerAnswered = false;
-    for(var i=0; i<Object.keys(players).length; i++){
-        everyPlayerAnswered += questionStatus[data.qID][Object.keys(players)[i]];
-    }
-
-    if(everyPlayerAnswered){
+    if(nbOfAnswers === Object.keys(players).length){
         console.log('Every player answered question '+data.qID);
-    }
+        //prevent clients to change answer
+        //show answer for question to clients
+        //compute & send scores
 
-    io.emit(EVENT_UPDATE_ANSWERS,{answers:answersCounters});
+        scores = {};
+
+        let qNumber = data.qID.replace('q','') - 1;
+
+        io.emit(EVENT_EVERY_PLAYER_ANSWERED,{qID:data.qID,answer:quiz.quiz[qNumber].answer});
+
+        for(let i=0; i<Object.keys(players).length; i++){
+            let uID = Object.keys(players)[i];
+            var score = 0;
+
+            for(let qID=0;qID<quiz.quiz.length;qID++){
+                let question = quiz.quiz[qID];
+                let userAnswer = quiz.quiz[qID].options[playersAnswers[question.id][uID]];
+                if(userAnswer === question.answer){
+                    score++;
+                }
+            }
+            players[uID].score = score;
+        }
+
+        io.emit(EVENT_PLAYERS,{players:players});
+    }
 }
 
 function loggedIn(data,socket){
     console.log("login de "+data["nickname"]);
 
     players[socket.id] = {id:socket.id,nickname:data['nickname'],score:0};
+    io.emit(EVENT_PLAYERS,{players:players});
 
     for (let questionIdx = 0; questionIdx < (quiz.length); questionIdx++) {
         //extract content from the question
         question = quiz[questionIdx];
         questionId = question.id;
-
-        questionStatus[questionId][socket.id] = false;
     }
 
-    io.emit(EVENT_PLAYERS,{players:players});
-
-    //socket.on(EVENT_DESELECT, deselected);
     socket.on(EVENT_ANSWER, answered);
 
-    //send the questions to the client
-    socket.emit("quiz", quiz);
+
+    if(clientCount === Object.keys(players).length){
+        //send the questions to all clients
+        io.emit(EVENT_QUIZ, quiz);
+    }else{
+        socket.emit(EVENT_WAITING_PAGE);
+    }
+}
+
+function testNickname(data, socket) {
+    var nickTaken = 0;
+
+    for(let i=0; i<Object.keys(players).length; i++) {
+        let player = players[Object.keys(players)[i]];
+        nickTaken += (data.nickname === player.nickname);
+        console.log(data.nickname, player.nickname, nickTaken);
+    }
+
+    socket.emit(EVENT_NICKNAME_TEST_RESPONSE,{nicknameTaken:nickTaken,nickname:data.nickname});
 }
 
 server.listen(8080,"0.0.0.0");
